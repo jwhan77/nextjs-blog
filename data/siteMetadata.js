944d1cf8a02309aca46e5d8d4baa81031940682f
jwhan77 <gksjeongw96@gmail.com>
2022-05-15 18:55:47 +0900

    Enable umami analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -23,7 +23,7 @@ const siteMetadata = {
     // supports plausible, simpleAnalytics, umami or googleAnalytics
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    umamiWebsiteId: '73d14ae0-7b2c-49e0-8004-77fb0e5d0799', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
